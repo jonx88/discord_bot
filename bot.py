dd1cfc07ba58a1bbef61336253c42c837849b453
Jon Xie <jon.xie@aderant.com>
2022-01-09 19:18:11 -0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+from discord.ext import commands
+
+def main():
+
+    TOKEN_JON = "ODcwMDU5NTM4NDk2MjI5NDE3.YQHQFg.99-Z31piEWQu8hLId2egjCVeblU"
+
+    token = os.getenv("DISCORD_TOKEN")
+    client = commands.Bot(command_prefix="?")
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user.name} is onnected to Discord")
+
+    @client.event
+    async def on_message(ctx):
+        if(ctx.content.startswith("hello")):
+            await ctx.channel.send("Hi")
+            
+    client.run(TOKEN_JON)
+
+
+# entry point
+if __name__ == '__main__':
+    main()
